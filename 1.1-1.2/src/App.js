a5881c237ee735788038095cd0b775873e83840c
arjundesguerra <arjundesguerra@gmail.com>
2022-12-21 21:18:20 -0800

    1.2

--- 1.1-1.2/src/App.js
+++ 1.1-1.2/src/App.js
@@ -1,3 +1,13 @@
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+      {props.part} {props.exercises}
+      </p>
+    </div>
+  )
+}
+
 const Header = (props) => {
   return (
     <h1>{props.course}</h1>
@@ -7,15 +17,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-    <p>
-      {props.part1} {props.exercises1}
-    </p>
-    <p>
-      {props.part2} {props.exercises2}
-    </p>
-    <p>
-      {props.part3} {props.exercises3}
-    </p>
+      <Part part = {props.part1} exercises = {props.exercises1}/>
+      <Part part = {props.part2} exercises = {props.exercises2}/>
+      <Part part = {props.part3} exercises = {props.exercises3}/>
     </div>
   )
 }
